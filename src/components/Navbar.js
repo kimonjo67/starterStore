7fcd25e1f8e3ae9f93ef44af1ffe7e2f3e71a2dd
sam mwangi <monjokim@gmail.com>
2019-03-19 00:52:11 -0500

    App set up

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className= "navbar navbar-expand-sm navbar-dark px-sm-5">
+                
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
