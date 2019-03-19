e00d25a88b2b7a418c17cbce4230d144cf72a0d2
sam mwangi <monjokim@gmail.com>
2019-03-19 01:26:09 -0500

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import logo from '../logo.svg';
 
 class Navbar extends Component {
     render() {
         return (
-            <nav className= "navbar navbar-expand-sm navbar-dark px-sm-5">
-                
+            <nav className= "navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                {/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+                https://www.iconfinder.com/Makoto_msk */}
+                <Link to='/'>
+                    <img src={logo} alt="store" className="navbar-brand"/>
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5"></li>
+                    <Link to='/' className="nav-link">
+                        products
+                    </Link>
+                </ul>
             </nav>
         );
     }
