ce862019daa9c21c3db2a25bc95355a00fa20d6f
sam mwangi <monjokim@gmail.com>
2019-03-19 19:05:57 -0500

    finished nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import {ButtonContainer} from './Button';
 class Navbar extends Component {
     render() {
         return (
-          <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+          <NavWrapper className = "navbar navbar-expand-sm bg-primary navbar-dark px-sm-5" >
             {/* https://www.iconfinder.com/icons/1243689/call_phone_icon Creative Commons (Attribution 3.0 Unported);
                 https://www.iconfinder.com/Makoto_msk */}
             <Link to="/">
@@ -27,9 +27,21 @@ class Navbar extends Component {
                 my cart
               </ButtonContainer>
             </Link>
-          </nav>
+          </NavWrapper>
         );
     }
 }
+
+
+
+const NavWrapper = styled.nav`
+background:var(--mainBlue)! important;
+.nav-link{
+    color:var(--mainWhite)! important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+}
+
+`;
 
 export default Navbar;
